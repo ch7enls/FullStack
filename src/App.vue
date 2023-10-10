1c95d65c808b8bfc061e4bad05a39d4ca04d44d0
bigxx <1158469495@qq.com>
2023-10-10 16:04:37 +0800

    实现增删查改

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,41 @@
 <script setup>
-import {ref} from "vue";
+import { ref } from "vue";
 import request from "./utils/request.js";
 
-const name = ref("");
-const age = ref("");
+const content = ref("");
+const id = ref("");
 
-const login = async () => {
-  const res = await request("post", "/login");
+// 查询
+const search = async () => {
+  const res = await request("get", "/get-data");
+  console.log(res);
+};
+
+// 新增
+const add = async () => {
+  const data = {
+    content: content.value,
+  };
+  const res = await request("post", "/insert-data", data);
+  console.log(res);
+};
+
+// 修改
+const update = async () => {
+  const data = {
+    id: id.value,
+    content: content.value,
+  };
+  const res = await request("put", "/update-data", data);
+  console.log(res);
+};
+
+// 删除
+const deleted = async () => {
+  const data = {
+    id: id.value,
+  };
+  const res = await request("put", "/delete-data", data);
   console.log(res);
 };
 </script>
@@ -14,11 +43,14 @@ const login = async () => {
 <template>
   <h1>这是个测试，发起post请求，拿到node返回的数据。</h1>
 
-  <input type="text" name="name" />
-  <input type="text" name="age" />
-  <input type="submit" value="提交" @click="login()" />
+  content: <input type="text" name="content" v-model="content" /> id:
+  <input type="text" name="id" v-model="id" />
+  <input type="submit" value="查询" @click="search()" />
+  <input type="submit" value="新增" @click="add()" />
+  <input type="submit" value="修改" @click="update()" />
+  <input type="submit" value="删除" @click="deleted()" />
 
-  <div>{{ res }}</div>
+  <div style="height: 100px">{{ res }}</div>
 </template>
 
 <style scoped></style>
